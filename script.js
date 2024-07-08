26dcc6114420309ebf3d33ef8e6031d692fc6b3d
nandysumit91 <146019889+nandysumit91@users.noreply.github.com>
2024-07-08 18:59:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -30,19 +30,19 @@ async function checkWeather(city) {
 
     switch(weather_data.weather[0].main){
         case 'Clouds':
-            weather_img.src = "assets/cloud.png";
+            weather_img.src = "cloud.png";
             break;
         case 'Clear':
-            weather_img.src = "assets/clear.png";
+            weather_img.src = "clear.png";
             break;
         case 'Rain':
-            weather_img.src = "assets/rain.png"
+            weather_img.src = "rain.png"
             break;
         case 'Mist':
-            weather_img.src = "assets/mist.png";
+            weather_img.src = "mist.png";
             break;
         case 'Snow':
-            weather_img.src = "assets/snow.png";
+            weather_img.src = "snow.png";
             break;
     }
 }
